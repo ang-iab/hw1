2958e99dfd2a59dd828aeb11a45c7f1ee2b07641
Ang Qi <angqi@usc.edu>
2025-09-07 18:52:39 +0000

    Part 2 Completed

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,154 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+std::string const & ULListStr::back() const
+{
+  return tail_->val[tail_->last-1];
+}
+
+std::string const & ULListStr::front() const
+{
+  return head_->val[head_->first];
+}
+
+void ULListStr::push_back(const std::string& val)
+{
+  if(size_ == 0)
+  {
+      Item* newItem = new Item();
+      newItem->prev = nullptr;
+      newItem->next = nullptr;
+      newItem->first = 0;
+      newItem->last = 1;
+      newItem->val[0] = val;
+      head_ = newItem;
+      tail_ = newItem;
+  }
+  else
+  {
+    Item* temp = tail_;
+    if (temp->last == ARRSIZE)
+    {
+      Item* newItem = new Item();
+      newItem->prev = temp;
+      newItem->next = nullptr;
+      newItem->first = 0;
+      newItem->last = 1;
+      newItem->val[0] = val;
+      temp->next = newItem;
+      tail_ = newItem;
+    }
+    else
+    {
+      temp->val[temp->last] = val;
+      ++temp->last;
+    }
+  }
+  ++size_;
+}
+
+void ULListStr::pop_back()
+{
+  Item* temp = tail_;
+  temp->val[temp->last-1].clear();
+  --temp->last;
+  --size_;
+
+  if (temp->last - temp->first == 0)
+  {
+    Item* old = tail_;
+    tail_ = temp->prev;
+    if(tail_ != nullptr)
+    {
+      tail_->next = nullptr;
+    }
+    else
+    {
+      head_ = nullptr;
+    }
+    delete old;
+  }
+}
+
+void ULListStr::push_front(const std::string& val)
+{
+  if(size_ == 0)
+  {
+      Item* newItem = new Item();
+      newItem->prev = nullptr;
+      newItem->next = nullptr;
+      newItem->first = ARRSIZE - 1;
+      newItem->last = ARRSIZE;
+      newItem->val[ARRSIZE-1] = val;
+      head_ = newItem;
+      tail_ = newItem;
+  }
+  else
+  {
+    Item* temp = head_;
+    if (temp->first == 0)
+    {
+      Item* newItem = new Item();
+      newItem->prev = nullptr;
+      newItem->next = temp;
+      newItem->first = ARRSIZE - 1;
+      newItem->last = ARRSIZE;
+      newItem->val[ARRSIZE-1] = val;
+      temp->prev = newItem;
+      head_ = newItem;
+    }
+    else
+    {
+      temp->val[temp->first-1] = val;
+      --temp->first;
+    }
+  }
+  ++size_;
+}
+
+void ULListStr::pop_front()
+{
+  Item* temp = head_;
+  temp->val[temp->first].clear();
+  ++temp->first;
+  --size_;
+
+  if (temp->last - temp->first == 0)
+  {
+    Item* old = head_;
+    head_ = temp->next;
+    if (head_ != nullptr)
+    {
+      head_->prev = nullptr;
+    }
+    else
+    {
+      tail_ = nullptr;
+    }
+    delete old;
+  }
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+  size_t counter = 0;
+  std::string* ptr = nullptr;
+  Item* temp = head_;
+
+  while (counter <= loc)
+  {
+    for (size_t i = temp->first; i < temp->last; i++)
+    {
+      if (counter == loc)
+      {
+        ptr = &(temp->val[i]);
+        return ptr;
+      }
+      ++counter;
+    }
+    temp = temp->next;
+  }
+
+  return ptr;
+}
